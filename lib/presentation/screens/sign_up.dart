baaaa30b1fa651ba7a832e4378818c23af4a4c4b
FerSanchez95 <fersanchezz95@gmail.com>
2025-10-19 00:58:10 -0300

    Implementa la lógica de registro de usuario con validación de campos. Se agrega un SnackBar para mostrar mensajes de éxito o error al registrar. Se refactoriza la interfaz de usuario para incluir un formulario de registro.

--- lib/presentation/screens/sign_up.dart
+++ lib/presentation/screens/sign_up.dart
@@ -13,7 +13,7 @@ class SignUp extends StatefulWidget {
 
 class _AppSignUp extends State<SignUp> {
 
-
+  //Instance for validation
   FieldValidation validation = FieldValidation();
 
   late TextEditingController nameController = TextEditingController();
@@ -25,12 +25,39 @@ class _AppSignUp extends State<SignUp> {
   String _inputPassword = '';
   String _inputConfirmedPassword = '';
   
-  SnackBar snackBar = SnackBar(content: Text('Usuario registrado'));
+
+
+  final _formkey = GlobalKey<FormState>();
+
+  void _submitRegister(){
+    
+    String snackBarMessage = '';
+    
+
+    if (!_formkey.currentState!.validate()){ 
+      snackBarMessage = 'No se pudo registrar el usuario'; 
+    } else {
+      snackBarMessage = '¡Usuario registrado!';
+    }
+    SnackBar snackBar = SnackBar(content: Text(snackBarMessage));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
 
+    
+    
+    final EdgeInsets textFieldsInset = EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0);
+    
     final textStyle = Theme.of(context).textTheme;
+    final TextStyle titleStyle = TextStyle(
+      fontSize: textStyle.titleLarge?.fontSize ?? 20.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+      letterSpacing: 0.2,
+    );
+
 
     return Scaffold(
       appBar: AppBar(
@@ -42,130 +69,152 @@ class _AppSignUp extends State<SignUp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            Container(
-              margin: EdgeInsets.all(20.0),
-              width: double.infinity,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 43, 171, 245),
-                borderRadius: BorderRadius.all(Radius.circular(15.0))
-              ),
+            _RegisterTitleWidget(titleStyle: titleStyle),
+            
+            Form(
+              key: _formkey,
               child: Column(
                 children: [
-                  Column(
-
-                    children: [
-                    Image.asset('lib/images/signup_banner.png',fit: BoxFit.contain, scale: 5.0,),
-                  ]),
-
-                  Column(
-                    children: [
-                      Center(child: Text('Registro de usuario', style: textStyle.titleLarge)),
-                    ],
-                  )
+                
+                  // User name
+                  Container(
+                    margin: textFieldsInset,
+                    child: TextFormField(
+                      controller: nameController,
+                      onChanged: (String nameValue) {
+                        if (nameController.text.isNotEmpty){
+                          setState(() {
+                            _inputName = nameController.text;
+                          });
+                        }                
+                      },
+                      obscureText: false,
+                      decoration: InputDecoration(border: OutlineInputBorder(), 
+                                                  labelText: 'Nombre de usuario'), 
+                    ),
+                  ),
+                
+                  //Email
+                  Container(
+                    margin: textFieldsInset,
+                    child: TextFormField(
+                      controller: emailController,
+                      onChanged: (String mailValue) {
+                        if (emailController.text.isNotEmpty){
+                          setState(() {
+                            _inputEmail = emailController.text;
+                          });
+                        }                
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(), 
+                        labelText: 'E-Mail',
+                        ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (String? email) { return validation.validateEmail(email); },
+                    ),
+                  ),
+                
+                  //Password
+                  Container(
+                    margin: textFieldsInset,
+                    child: TextFormField(
+                      controller: passwordController,
+                      onChanged: (String passwordValue) {
+                        if (passwordController.text.isNotEmpty){
+                          setState(() {
+                            _inputPassword = passwordController.text;
+                          });
+                        }                
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(), 
+                        labelText: 'Contraseña',
+                        ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (String? password) { return validation.validatePassword(password); },
+                    ),
+                  ),
+                
+                  //Confirm password
+                  Container(
+                    margin: textFieldsInset,
+                    child: TextFormField(
+                      controller: confirmedPassController,
+                      onChanged: (String confirmedPasswordValue) {
+                        if (confirmedPassController.text.isNotEmpty){
+                          setState(() {
+                            _inputConfirmedPassword = confirmedPassController.text;
+                          });
+                        }                
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(), 
+                        labelText: 'Confirmar contraseña',
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (String? confirmedPassword) { 
+                        if (_inputPassword.isEmpty){
+                          return 'Primero ingrese una contraseña válida.';
+                        }
+                        return validation.confirmPassword(_inputPassword, confirmedPassword); 
+                      },
+                    ),
+                  ),
+                
+                  SizedBox(height: 20.0),
+                
+                  ElevatedButton(
+                    onPressed: (){ _submitRegister(); }, 
+                    style: ElevatedButton.styleFrom( backgroundColor: Colors.blue[400]),
+                    child: Text('Registrarse', style: TextStyle(color: Colors.white),),
+                  ),
+                    
                 ],
               ),
             ),
-
-
-            // User name
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: TextFormField(
-                controller: nameController,
-                onChanged: (String nameValue) {
-                  if (nameController.text.isNotEmpty){
-                    setState(() {
-                      _inputName = nameController.text;
-                    });
-                  }                
-                },
-                obscureText: false,
-                decoration: InputDecoration(border: OutlineInputBorder(), 
-                                            labelText: 'Nombre de usuario'), 
-              ),
-            ),
-
-            //Email
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: TextFormField(
-                controller: emailController,
-                onChanged: (String mailValue) {
-                  if (emailController.text.isNotEmpty){
-                    setState(() {
-                      _inputEmail = emailController.text;
-                    });
-                  }                
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(), 
-                  labelText: 'E-Mail',
-                  ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? email) { return validation.validateEmail(email); },
-              ),
-            ),
-
-            //Password
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: TextFormField(
-                controller: passwordController,
-                onChanged: (String passwordValue) {
-                  if (passwordController.text.isNotEmpty){
-                    setState(() {
-                      _inputPassword = passwordController.text;
-                    });
-                  }                
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(), 
-                  labelText: 'Contraseña',
-                  ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? password) { return validation.validatePassword(password); },
-              ),
-            ),
-
-            //Confirm password
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: TextFormField(
-                controller: confirmedPassController,
-                onChanged: (String confirmedPasswordValue) {
-                  if (confirmedPassController.text.isNotEmpty){
-                    setState(() {
-                      _inputConfirmedPassword = confirmedPassController.text;
-                    });
-                  }                
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(), 
-                  labelText: 'Confirmar contraseña',
-                  ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? confirmedPassword) { 
-                  if (_inputPassword.isEmpty){
-                    return 'Primero ingrese una contraseña válida.';
-                  }
-                  return validation.confirmPassword(_inputPassword, confirmedPassword); 
-                },
-              ),
-            ),
-
-            SizedBox(height: 20.0),
-            //TODO: This button should trigger the database resgistration.
-            FilledButton(onPressed: (){
-              
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            }, child: Text('Registrarse')),
           ]
         ),
       )
+    );
+  }
+}
+
+class _RegisterTitleWidget extends StatelessWidget {
+  const _RegisterTitleWidget({
+    super.key,
+    required this.titleStyle,
+  });
+
+  final TextStyle titleStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      width: double.infinity,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 43, 171, 245),
+        borderRadius: BorderRadius.all(Radius.circular(15.0))
+      ),
+      child: Column(
+        children: [
+          Column(
+    
+            children: [
+            Image.asset('lib/images/signup_banner.png',fit: BoxFit.contain, scale: 5.0,),
+          ]),
+    
+          Column(
+            children: [
+              Center(child: Text('Registro de usuario', style: titleStyle)),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
