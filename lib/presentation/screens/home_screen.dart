85aa311b9b275778070db81e21ddc6f4dc977be0
FerSanchez95 <fersanchezz95@gmail.com>
2025-11-06 22:12:47 -0300

    Implementa la funcionalidad de cierre de sesión en la pantalla de inicio, incluyendo manejo de errores y notificaciones.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,9 +1,12 @@
+import 'package:changas_ya_app/Domain/Auth_exception/auth_exception.dart';
 import 'package:changas_ya_app/presentation/providers/navigation_provider.dart';
 import 'package:changas_ya_app/presentation/screens/jobs_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:changas_ya_app/presentation/screens/profile_screen.dart';
+import 'package:changas_ya_app/core/Services/user_auth_controller.dart';
 import 'package:changas_ya_app/presentation/screens/nosotros_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -11,6 +14,33 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedIndex = ref.watch(selectedTabIndexProvider);
+    final UserAuthController authController = UserAuthController();
+
+    void snackBarPopUp(String message, Color? background) {
+      SnackBar snackBar = SnackBar(
+        content: Text(message),
+        backgroundColor: background,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+    Future<bool> userLogOut() async {
+      String snackBarMessage = '';
+      Color? snackBarColor = Colors.red[400];
+      bool isLogedOut = false;
+
+      try {
+        await authController.userLogOut();
+        isLogedOut = true;
+        snackBarMessage = "Sesión cerrada.";
+        snackBarColor = Colors.green[400];
+      } on AuthException catch (e) {
+        snackBarMessage = e.showError();
+      }
+
+      snackBarPopUp(snackBarMessage, snackBarColor);
+      return isLogedOut;
+    }
 
     final List<Widget> screens = [
       const JobsScreen(),
@@ -20,9 +50,7 @@ class HomeScreen extends ConsumerWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Changas Ya!'),
-      ),
+      appBar: AppBar(title: const Text('Changas Ya!')),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -46,9 +74,7 @@ class HomeScreen extends ConsumerWidget {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (_) => const NosotrosScreen(),
-                  ),
+                  MaterialPageRoute(builder: (_) => const NosotrosScreen()),
                 );
               },
             ),
@@ -56,18 +82,18 @@ class HomeScreen extends ConsumerWidget {
             ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Cerrar sesión'),
-              onTap: () {
-                // TODO: implementar logout cuando esté lista la funcionalidad
-                Navigator.pop(context);
+              onTap: () async {
+                bool isSessionClosed = await userLogOut();
+                if (isSessionClosed && context.mounted) {
+                  Navigator.pop(context); // <-- cierra el drawner menu.
+                  context.go("/login");
+                }
               },
             ),
           ],
         ),
       ),
-      body: IndexedStack(
-        index: selectedIndex,
-        children: screens,
-      ),
+      body: IndexedStack(index: selectedIndex, children: screens),
       bottomNavigationBar: NavigationBar(
         selectedIndex: selectedIndex,
         onDestinationSelected: (index) {
