3e23e44c4e06fb0a11fc08432729fb7a3d244680
alonsojmateo <alonso.jmateo@gmail.com>
2025-11-09 16:22:58 -0300

    Se agrega refactor en submit rating

--- lib/core/data/rating_repository.dart
+++ lib/core/data/rating_repository.dart
@@ -1,9 +1,11 @@
 import 'package:changas_ya_app/Domain/Rating/rating.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class RatingRepository {
   final FirebaseFirestore _db;
   final String _collectionName = 'puntuaciones';
+  final String _collectionProfileName = 'usuarios';
 
   RatingRepository(this._db);
 
@@ -12,13 +14,48 @@ class RatingRepository {
     // setteamos manualmente el id
     final String ratingId = '${rating.jobId}_${rating.reviewerId}';
 
+    final ratingRef = _db.collection(_collectionName).doc(ratingId);
+    final profileRef = _db.collection(_collectionProfileName).doc(rating.reviewedId);
+
     try {
-      await _db.collection(_collectionName)
-      .doc(ratingId)
-      .set(
-        rating.toFirestore(),
-        SetOptions(merge: true)
-      );
+      await _db.runTransaction((transaction) async {
+        final profileSnapshot = await transaction.get(profileRef);
+        final ratingSnapshot = await transaction.get(ratingRef);
+
+        // En los dos casos puede ser null la data, y despues puede que no tenga opiniones entonces
+        // le pongo el ? y el ternario para que sea 0 sino
+        double actualSum = (profileSnapshot.data()?['ratingSum'] as num?)?.toDouble() ?? 0.0;
+        int actualCount = profileSnapshot.data()?['ratingCount'] as int? ?? 0;
+
+        double oldScore = 0.0;
+        int countChange = 0;
+
+        if(ratingSnapshot.exists) {
+          // Cuando existe un rating anterior tengo que restarlo de la cuenta total por eso setteo el oldScore
+          oldScore = (ratingSnapshot.data()?['score'] as num).toDouble();
+        } else {
+          oldScore = 0.0;
+          countChange = 1;
+        }
+
+        final double scoreChange = rating.score - oldScore;
+        final double finalSum = actualSum + scoreChange;
+        final int finalCount = actualCount + countChange;
+
+        final double average = finalCount / finalSum;
+
+        transaction.set(
+          ratingRef,
+          rating.toFirestore(),
+          SetOptions(merge: true),
+        );
+
+        transaction.update(profileRef, {
+          'ratingSum': finalSum,
+          'ratingCount': finalCount,
+          'ratingAvg': average,
+        });
+      });
 
     } on FirebaseException catch(e) {
       throw Exception('Fallo al subir nuevo rating');
@@ -39,31 +76,15 @@ class RatingRepository {
 
   Future<double> getAverageRating(String reviewedId) async {
     try {
-      final ratings = await _db.collection(_collectionName)
-        .where('reviewedId', isEqualTo: reviewedId)
-        .get();
-
-      if(ratings.docs.isEmpty) {
-        return 0.0;
+      final doc = await _db.collection(_collectionProfileName).doc(reviewedId).get();
+      if (doc.exists) {
+        final double? average = (doc.data()?['averageRating'] as num?)?.toDouble();
+        return average ?? 0; // average o 0 si no existe el average
       }
-
-      double scoreSum = 0.0;
-
-      for (var doc in ratings.docs) {
-        final score = doc.data()['score'];
-        // me aseguro de que no haya basura
-        if(score is num) {
-          scoreSum += score.toDouble();
-        }
-      }
-
-      final average = scoreSum / ratings.docs.length;
-
-      return double.parse(average.toStringAsFixed(1)); // un solo decimal, convierte y convierte
+      return 0.0; // doc no existe
     } on FirebaseException catch(e) {
       print("Error al calcular promedio");
       return 0.0;
     }
-
   }
 }
